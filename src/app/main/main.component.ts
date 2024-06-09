82eb3b1c97534d94595c28a8342c314fbc0e0b07
unknown <j.quabeck@gmx.net>
2024-06-09 21:10:41 +0200

    added footer and header

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
