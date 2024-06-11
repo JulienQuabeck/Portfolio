2eb277a50761288747aa3d32bffae6f1eb43f4f4
unknown <j.quabeck@gmx.net>
2024-06-11 19:24:56 +0200

    added socialMedia

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../landingPage/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialMediaComponent } from '../shared/social-media/social-media.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SocialMediaComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
