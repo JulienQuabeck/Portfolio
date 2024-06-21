f00f01f40d83b5d6327c28ab574983d318278cab
unknown <j.quabeck@gmx.net>
2024-06-21 23:20:30 +0200

    contact

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { SelfPresentationComponent } from '../self-presentation/self-presentation.component';
 import { SkillsetComponent } from '../skillset/skillset.component';
 import { MyWorkComponent } from '../my-work/my-work.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent, LandingPageComponent, SelfPresentationComponent, SkillsetComponent, MyWorkComponent],
+  imports: [FooterComponent, LandingPageComponent, SelfPresentationComponent, SkillsetComponent, MyWorkComponent, ContactMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
