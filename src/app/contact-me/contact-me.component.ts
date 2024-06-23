fcd5255079d4a9645094c6bfeed36baba776cfe3
unknown <j.quabeck@gmx.net>
2024-06-23 15:52:23 +0200

    desgin finished

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+  checked = false;
+
+  checkCheckbox(){
+    if(this.checked == false){
+      this.checked = true;
+    }else{
+      this.checked = false;
+    }
+  }
 
 }
