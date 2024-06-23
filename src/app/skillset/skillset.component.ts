9bd1cd393824edeb94566796daaebbc22d6f1b77
unknown <j.quabeck@gmx.net>
2024-06-23 10:15:59 +0200

    added some specials

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -11,16 +11,45 @@ import { Component } from '@angular/core';
 export class SkillsetComponent {
 
   skills = [
-    './assets/img/skillset/html.png',
-    './assets/img/skillset/css.png',
-    './assets/img/skillset/javascript.png',
-    './assets/img/skillset/github.png',
-    './assets/img/skillset/restapi.png',
-    './assets/img/skillset/scrum.png',
-    './assets/img/skillset/typescript.png',
-    './assets/img/skillset/angular.png',
-    './assets/img/skillset/firebase.png',
-    './assets/img/skillset/materialdesign.png'
+    {
+      name: 'HTML',
+      url: './assets/img/skillset/html.png',
+    },
+    {
+      name: 'CSS',
+      url: './assets/img/skillset/css.png',
+    },
+    {
+      name: 'JavaScript',
+      url: './assets/img/skillset/javascript.png',
+    },
+    {
+      name: 'GitHub',
+      url: './assets/img/skillset/github.png',
+    },
+    {
+      name: 'API',
+      url: './assets/img/skillset/restapi.png',
+    },
+    {
+      name: 'SCRUM',
+      url: './assets/img/skillset/scrum.png',
+    },
+    {
+      name: 'TypeScript',
+      url: './assets/img/skillset/typescript.png',
+    },
+    {
+      name: 'Angular',
+      url: './assets/img/skillset/angular.png',
+    },
+    {
+      name: 'Firebase',
+      url: './assets/img/skillset/firebase.png',
+    },
+    {
+      name: 'Materialdesign',
+      url: './assets/img/skillset/materialdesign.png'
+    },
   ];
-
 }
