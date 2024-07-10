69eec3e2cea5bf601fd8e3f4b117fc782841a985
unknown <j.quabeck@gmx.net>
2024-07-10 21:06:25 +0200

    contact form

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,22 +1,78 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+  http = inject(HttpClient)
+
   checked = false;
 
-  checkCheckbox(){
-    if(this.checked == false){
+  checkCheckbox() {
+    if (this.checked == false) {
       this.checked = true;
-    }else{
+    } else {
       this.checked = false;
     }
+  }
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  nameValid = false;
+  emailValid = false;
+  messageValid = false;
+  checkboxChecked = false;
+  isFocused: any = {'name': false, 'email': false, 'message': false};
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            //hier alles weitere einfügen (falls gewünscht)
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      //hier alles weitere einfügen (falls gewünscht)
+      ngForm.resetForm();
+    }
+  }
+
+  onFocus(id:string) {
+    this.isFocused[id] = true;
+  }
+
+  onBlur(id: string) {
+    this.isFocused[id] = false;
   }
 
 }
