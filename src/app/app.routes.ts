3adb8d675f3c698a8c976d278ca176668e49cf97
unknown <j.quabeck@gmx.net>
2024-07-14 14:18:03 +0200

    added privacy Policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     {path: '',component: MainComponent},
     {path: 'landing-Page', component: MainComponent},
-    {path: 'Imprint',component: ImprintComponent}
+    {path: 'Imprint',component: ImprintComponent},
+    {path: 'Privacy-Policy', component: PrivacyPolicyComponent}
 ];
